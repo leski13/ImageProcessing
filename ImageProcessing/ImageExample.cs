d3ddf2d0492243d4231128805f48785f767658a9
leski139013 <leski139013@gmail.com>
2020-10-25 11:18:05 +0200

    Знакомство с Emgu

--- ImageProcessing/ImageExample.cs
+++ ImageProcessing/ImageExample.cs
@@ -7,16 +7,6 @@ namespace ImageProcessing
 {
     public class ImageExample
     {
-        public static string[] GetImagesWords()
-        {
-            List<string> w = new List<string>();
-
-            using(ImageParser ip = new ImageParser(@"C:\emgu\bin\testdata", "eng"))
-            {
-                if (ip.OcrImage(@"D:\ФОТКИ\телега\235137262_188332.jpg") != string.Empty)
-                    w.AddRange(ip?.Words.ToList<string>());
-            }
-            return w.ToArray();
-        }
+        
     }
 }
